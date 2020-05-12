9269eca36d7d8d4867511ed8cad61caa2c1ccfd2
xumwen <421269175@qq.com>
2020-05-12 10:23:35 +0800

    modify pems percent=0.5

--- preprocess.py
+++ preprocess.py
@@ -200,7 +200,7 @@ def load_pems_d7_data(directory="data/PeMS-D7"):
     X = np.load(directory + "/node_values.npy")
     X = X.astype(np.float32)
     # to avoid OOM and only load a part
-    percent = 0.1
+    percent = 0.5
     X = X[:, :, :int(percent * X.shape[2])]
     print('(num_nodes, num_features, num_timesteps) is ', X.shape)
     
